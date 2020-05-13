55538b7b65372209111a41af885d6994a1cb5373
aiden5070 <46075799+aiden5070@users.noreply.github.com>
2020-05-13 13:32:51 -0400

    Create serverscript.lua

---
+++ serverscript.lua
@@ -0,0 +1,19 @@
+local modules = game:GetService('ReplicatedStorage'):FindFirstChild('MainModules')
+if not modules then
+  error("Missing folder. Please re-install.")
+end
+
+function sudo_require(mod)
+    if mod:IsA("StringValue") then
+        -- >> use loadstring module that was downloaded
+        local ls = require(modules:WaitForChild("Loadstring"))
+        return ls(mod.Value)() -- << return value
+    end
+end
+
+local Dep = game:GetService('ReplicatedStorage'):FindFirstChild('Dep')
+if not dep then
+  error("Missing folder. Please re-install.")
+end
+
+sudo_require(dep:WaitForChild("Main"))
